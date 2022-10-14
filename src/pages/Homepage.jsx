d5d747bb04cfb0fdd3da1f898907a6bb79d5176c
TheroneAlmadin <80694204+TheroneAlmadin@users.noreply.github.com>
2022-10-15 01:43:33 +0800

    Sidebar- issue fix
    the problem left is the white thingy not collapsing

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -14,10 +14,10 @@ import {
 
 const Homepage = () => {
 
+  const activeMenu = true;
   const {
     setCurrentColor,
     setCurrentMode,
-    activeMenu,
     currentMode,
     currentColor,
     themeSettings,
@@ -49,20 +49,20 @@ const Homepage = () => {
         </TooltipComponent>
         </div>     
           {activeMenu ?( 
-            <div className="fixed bg-white w-72 sidebar dark: bg-secondary-dark-bg">
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
               <Sidebar/>
             </div>
-          ):(
+          ) : (
             <div className ="w-0 dark: bg-secondary-dark-bg">
               
             </div>
           )}
           <div className={
-            activeMenu ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full'
+            activeMenu ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full '
             : ' - main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
           }>
 
-            <div className="fixed w-full md:static bg-main-bg dark: bg-main-dark-bg navbar">
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
             <Navbar />
             </div>
           </div>
